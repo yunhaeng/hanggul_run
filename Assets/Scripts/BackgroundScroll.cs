bfb350d8b52572b2c699b4f264dfc644b8691ecf
yunhaeng <dkfk591@gmail.com>
2024-12-31 07:17:06 +0900

    [CRT] Add background scroll

---
+++ Assets/Scripts/BackgroundScroll.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class BackgroundScroll : MonoBehaviour
+{
+    [Header("Settings")]
+    [Tooltip("How fast should the background scroll?")]
+    public float scrollSpeed;
+
+    [Header("References")]
+    public MeshRenderer meshRenderer;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        meshRenderer.material.mainTextureOffset += new Vector2(scrollSpeed * Time.deltaTime, 0);
+    }
+}
