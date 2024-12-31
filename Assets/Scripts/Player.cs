6d814e43aa7464698ff411a4f72f4f3afe696c91
yunhaeng <dkfk591@gmail.com>
2024-12-31 14:17:27 +0900

    [CRT] Add Player Animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,7 +10,7 @@ public class Player : MonoBehaviour
 
     [Header("References")]
     public Rigidbody2D PlayerRigidbody;
-
+    public Animator PlayerAnimator;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -23,11 +23,16 @@ public class Player : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Space) && isGrounded==true){
             PlayerRigidbody.AddForceY(JumpForce, ForceMode2D.Impulse);
             isGrounded = false;
+            PlayerAnimator.SetInteger("state", 1);
         }
     }
 
     void OnCollisionEnter2D(Collision2D collision){
         if(collision.gameObject.name == "Platform"){
+            if (!isGrounded)
+            {
+                PlayerAnimator.SetInteger("state", 2);
+            }
             isGrounded = true;
         }
     }
